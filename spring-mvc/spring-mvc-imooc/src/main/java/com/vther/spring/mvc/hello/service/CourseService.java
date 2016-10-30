8171f2af99f0bd2eb6224ffc5d41a43d97f9a1cb
vther <vther@qq.com>
2016-10-30 19:43:39 +0800

    提交SpringMVC基础Demo，基于jetty可运行

---
+++ spring-mvc/spring-mvc-imooc/src/main/java/com/vther/spring/mvc/hello/service/CourseService.java
@@ -0,0 +1,10 @@
+package com.vther.spring.mvc.hello.service;
+
+import com.vther.spring.mvc.hello.model.Course;
+
+
+public interface CourseService {
+
+    Course getCourseById(Integer courseId);
+
+}
