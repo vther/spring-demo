b7c11f8a7fd533bf7e1d6e9f4c6298a0bb7ff763
vther <vther@qq.com>
2017-03-09 22:48:10 +0800

    提交jpa subquery 研究

--- spring-data-jpa/src/test/java/com/vther/spring/data/jpa/service/impl/CustomerServiceImplTest.java
+++ spring-data-jpa/src/test/java/com/vther/spring/data/jpa/service/impl/CustomerServiceImplTest.java
@@ -1,8 +1,37 @@
+package com.vther.spring.data.jpa.service.impl;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.powermock.api.mockito.PowerMockito;
+
 import static org.junit.Assert.*;
 
-/**
- * Created by Wither on 2017/3/9.
- */
+
 public class CustomerServiceImplTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void createCustomer() throws Exception {
+
+    }
+
+    @Test
+    public void findByAgeGreaterThan() throws Exception {
+
+    }
+
+    @Test
+    public void findAll() throws Exception {
+
+    }
 
 }
