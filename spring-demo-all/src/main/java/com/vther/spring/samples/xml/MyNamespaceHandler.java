4b60aa1ebef5f35a6896eed99f69d2532df98af1
vther <vther@qq.com>
2017-06-29 21:49:44 +0800

    提交XML扩展

---
+++ spring-demo-all/src/main/java/com/vther/spring/samples/xml/MyNamespaceHandler.java
@@ -0,0 +1,13 @@
+package com.vther.spring.samples.xml;
+
+
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+public class MyNamespaceHandler extends NamespaceHandlerSupport {
+
+    public void init() {
+        registerBeanDefinitionParser("dateformat", new SimpleDateFormatBeanDefinitionParser());
+    }
+
+}
